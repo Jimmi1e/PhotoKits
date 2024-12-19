a2256d74e578a173117f09af0d9150b4e47c18e5
Jiaxi Yang <144962751+Jimmi1e@users.noreply.github.com>
2024-12-18 23:58:26 -0500

    Add files via upload
    First Version v0.0.1

---
+++ PhotoKits/PhotoKits/PhotoKits/PhotoPicker.swift
@@ -0,0 +1,59 @@
+//
+//  PhotoPicker.swift
+//  PhotoKits
+//
+//  Created by Jason Young on 2024-12-17.
+//
+
+import SwiftUI
+import PhotosUI
+
+struct PhotoPicker: UIViewControllerRepresentable {
+    @Binding var selectedImage: UIImage?
+
+    func makeUIViewController(context: Context) -> PHPickerViewController {
+        var config = PHPickerConfiguration()
+        config.filter = .images
+        config.selectionLimit = 1
+        
+        let picker = PHPickerViewController(configuration: config)
+        picker.delegate = context.coordinator
+        return picker
+    }
+
+    func updateUIViewController(_ uiViewController: PHPickerViewController, context: Context) {}
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    class Coordinator: NSObject, PHPickerViewControllerDelegate {
+        let parent: PhotoPicker
+
+        init(_ parent: PhotoPicker) {
+            self.parent = parent
+        }
+
+        func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+            picker.dismiss(animated: true)
+            
+            // 先清理之前选择的图片
+            parent.selectedImage = nil
+            
+            guard let provider = results.first?.itemProvider else { return }
+            if provider.canLoadObject(ofClass: UIImage.self) {
+                provider.loadObject(ofClass: UIImage.self) { image, _ in
+                    DispatchQueue.main.async {
+                        // 更新为新图片
+                        self.parent.selectedImage = image as? UIImage
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+
+
+
